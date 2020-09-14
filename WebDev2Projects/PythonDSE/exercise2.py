68a3154a4c88715818a5236610c53352141ca0e0
caaiyen <ailyn.albores2000@gmail.com>
2020-09-14 12:05:35 +0800

    modify python file in Webdevexercises

--- WebDev2Projects/PythonDSE/exercise2.py
+++ WebDev2Projects/PythonDSE/exercise2.py
@@ -3,7 +3,17 @@ from time import sleep
 cp.pixels.brightness = 1
 
 
+color = 0
+index = 0
 while True:
+    if cp.light == 0:  
+        color = cp.light
+    elif cp.light > 0:
+        if(cp.light % 32 == 0):
+            index = (cp.light / 32) - 1
+        print(32 * cp.light/ 32)
     
-  
     
+    
+    #     color = int(255 * ()/32)
+    # cp.pixels[index] = (color,color,color) 
